7214a2f4b9e76cf2dfc7442e89facc66bfec2ac6
tonypera <paulurbina@hotmail.com>
2019-01-16 11:30:25 -0500

    add .env

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
+
 module.exports = {
 
     database: {
-        host: 'us-cdbr-iron-east-01.cleardb.net',
-        user: 'b5b0dc098c957a',
-        password: 'eff36c1f',
-        database: 'heroku_62a1f8c284e9910'
+        // host: 'us-cdbr-iron-east-01.cleardb.net',
+           host: 'HOST_DATABASE',
+        // user: 'b5b0dc098c957a',
+           user: 'DB_USERNAME',
+        // password: 'eff36c1f',
+           password: 'DB_PASSWORD',
+        // database: 'heroku_62a1f8c284e9910'
+           database: 'DATABASE_NAME'
     }
 
 };
