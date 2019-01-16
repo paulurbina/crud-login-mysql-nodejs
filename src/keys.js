167135ccf5be3e336dc285a78edd483a60040c8a
tonypera <paulurbina@hotmail.com>
2019-01-16 11:50:39 -0500

    make it better

--- src/keys.js
+++ src/keys.js
@@ -1,16 +1,27 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-module.exports = {
+const { Pool } = require('pg');
 
-    database: {
+// CONNECTING DATA BASE HEROKU
+const database = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+
+
+module.exports = database;
+
+// module.exports = {
+
+    // database: {
         // host: 'us-cdbr-iron-east-01.cleardb.net',
-           host: 'HOST_DATABASE',
+        //    host: 'HOST_DATABASE',
         // user: 'b5b0dc098c957a',
-           user: 'DB_USERNAME',
+        //    user: 'DB_USERNAME',
         // password: 'eff36c1f',
-           password: 'DB_PASSWORD',
+        //    password: 'DB_PASSWORD',
         // database: 'heroku_62a1f8c284e9910'
-           database: 'DATABASE_NAME'
-    }
+        //    database: 'DATABASE_NAME'
+//     }
 
-};
+// };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const { database } = require('../keys');
+
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+router.get('/db', async (req, res) => {
+    try {
+      const client = await database.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 
 module.exports = router;
