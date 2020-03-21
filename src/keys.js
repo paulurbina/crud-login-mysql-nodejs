bef69030ea06b4cb5d4727c53cc63c3f1a3e9e1d
paulurbina <purbina@cognitiva.com.pe>
2020-03-20 21:04:15 -0500

    change keys

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
    database: {
-      host: '35.227.35.244',
+      socketPath: 'test-project-267903:us-central1:myinstance',
       user: 'root',
       password: 'ikPyD42F5i9uw8N8',
       database: 'database_links'
